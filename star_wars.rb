b79d8d71dd4c292e3309d205eb2e906805a1f0a6
Daniel Le Cornu <dlec2@hotmail.co.uk>
2016-03-20 12:04:37 +0000

    removed unneeded helper

--- star_wars.rb
+++ star_wars.rb
@@ -5,13 +5,6 @@ require_relative 'states/menu_state'
 require_relative 'states/play_state'
 require_relative 'game_window'
 
-module Game
-	def self.media_path(file)
-		File.join(File.dirname(File.dirname(
-			__FILE__)), 'media', file)
-	end
-end
-
 $window = GameWindow.new
 GameState.switch(MenuState.instance)
 $window.show
